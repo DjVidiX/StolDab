6ea6a3ddf592993180afa6c140c890cf956fa192
DjVidiX <djvidix.ms@gmail.com>
2014-08-04 23:29:45 +0200

    prace nad strona główna

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,23 +1,29 @@
 'use strict';
 
 angular.module('webApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+.controller('NavbarController', function ($scope, $location, Auth) {
+    $scope.menu = [
+        {
+            'title': 'Home',
+            'link': '/'
+        },
+        {
+            'title': 'Działalność',
+            'link': '/offer'
+        }
+    ];
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
+    $scope.logout = function () {
+        Auth.logout();
+        $location.path('/login');
     };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
+    $scope.isActive = function (route) {
+        return route === $location.path();
     };
-  });
+});
