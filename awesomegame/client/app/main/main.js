26457e51108ed5a798d40bdb40503cad93d0ecdc
DjVidiX <djvidix.ms@gmail.com>
2014-08-04 20:55:47 +0200

    initial commit

---
+++ awesomegame/client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('awesomegameApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
